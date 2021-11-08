8432716bc598503ad70157676b020ea5a0d93884
Pratik Pednekar <Pratik_Pednekar@McAfee.com>
2021-11-08 08:26:58 +0530

    auto suggest

---
+++ src/components/AutoSuggest.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useState } from "react";
+import Autosuggest from 'react-autosuggest';
+import theme from '../css/AutoSuggest.scss'
+
+const AutoSuggest = (props) => {
+    
+    const [value, setValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+    const listNames = [
+        "list1", "list2", "list3"
+    ];
+    
+    // Teach Autosuggest how to calculate suggestions for any given input value.
+    const getSuggestions = value => {
+        console.log(value)
+        const inputValue = value.trim().toLowerCase();
+        const inputLength = inputValue.length;
+  
+        return inputLength === 0 ? [] : listNames.filter(val =>
+            val.toLowerCase().slice(0, inputLength) === inputValue
+        );
+    };
+
+    // When suggestion is clicked, Autosuggest needs to populate the input
+    // based on the clicked suggestion. Teach Autosuggest how to calculate the
+    // input value for every given suggestion.
+    const getSuggestionValue = (suggestion) => {
+        props.setParentValue(suggestion);
+        return suggestion;
+    }
+
+    // render suggestions.
+    const renderSuggestion = suggestion => (
+        <div>
+            {suggestion}
+        </div>
+    );
+
+    const onChange = (event, { newValue }) => {
+        setValue(newValue);
+    };
+
+    // Autosuggest will call this function every time you need to update suggestions.
+    // You already implemented this logic above, so just use it.
+    const onSuggestionsFetchRequested = ({ value }) => {
+        setSuggestions(getSuggestions(value));
+    };
+
+    // Autosuggest will call this function every time you need to clear suggestions.
+    const onSuggestionsClearRequested = () => {
+        setSuggestions([]);
+    };
+
+    const inputProps = {
+        placeholder: props.placeholder,
+        value,
+        onChange: onChange
+    };
+    
+    return (
+        <Autosuggest 
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+        />
+    )
+}
+
+export default AutoSuggest
--- src/components/CreateList.js
+++ src/components/CreateList.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import AutoSuggest from './AutoSuggest';
 import {  Button } from 'react-bootstrap'
 import Modal from "react-bootstrap/Modal";
+// import Form from "react-bootstrap/Form";
 import ModalBody from "react-bootstrap/ModalBody";
 import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
+import { Formik, Form, Field } from 'formik';
+import * as Yup from "yup";
+import { useState, useRef } from 'react'
 
 const CreateList = (props) => {
+
+  const inputRef = React.createRef();
+  const [listNameValue, setListNameValue] = useState('');
+
+  const setSelectedValue = (value) => {
+    console.log(inputRef)
+    console.log("selected value")
+    console.log(value)
+    setListNameValue(value);
+    inputRef.current.onChange();
+  }
+  
+  const validationSchema = Yup.object().shape({
+    listName: Yup.string()
+      .required("List name is required"),
+    listDescription: Yup.string()
+      .required("List description is required"),
+    tag: Yup.string()
+      .required("Tag is required"),
+    url: Yup.string()
+      .required("Url is required"),
+    urlDescription: Yup.string()
+      .required("Url description is required")
+  });
+  
   return (
 
     <Modal show={props.show} onHide={props.handleClose}>
@@ -14,37 +44,65 @@ const CreateList = (props) => {
         <ModalTitle>Post a link</ModalTitle>
       </ModalHeader>
       <ModalBody>
-        <form>
-          <div className="mb-3">
-            <label for="listName" className="form-label">List Name</label>
-            <input type="text" className="form-control" id="listName" aria-describedby="listNameHelp" />
-            <div id="listNameHelp" className="form-text">Your links are added to lists, please specify a list name</div>
-          </div>
-          <div className="mb-3">
-            <label for="listDescription" className="form-label">List Description</label>
-            <input type="text" className="form-control" id="listDescription" aria-describedby="listDescriptionHelp" />
-            <div id="listDescriptionHelp" className="form-text">What kind of content is present in this list?</div>
-          </div>
-          <div className="mb-3">
-            <label for="tag" className="form-label">Tag</label>
-            <input type="text" className="form-control" id="tag" aria-describedby="tagHelp" />
-            <div id="tagHelp" className="form-text">Select a tag name that best describes your list</div>
-          </div>
-          <div className="mb-3">
-            <label for="url" className="form-label">URL</label>
-            <input type="text" className="form-control" id="url" aria-describedby="urlHelp" />
-            <div id="urlHelp" className="form-text">Provide the link you want to share</div>
-          </div>
-          <div className="mb-3">
-            <label for="urlDescription" className="form-label">URL Description</label>
-            <input type="text" className="form-control" id="urlDescription" aria-describedby="urlDescriptionHelp" />
-            <div id="urlDescriptionHelp" className="form-text">What is the link about?</div>
-          </div>
-        </form>
+        <Formik
+          initialValues={{
+            listName: "",
+            listDescription: "",
+            tag: "",
+            url: "",
+            urlDescription: ""
+          }}
+          validationSchema={validationSchema}
+          onSubmit={values => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
+
+          {({ errors, touched, handleChange }) =>
+          (
+            <Form>
+              <div className="mb-3">
+                <label for="listName" className="form-label">List Name</label>
+                <AutoSuggest setParentValue={setSelectedValue}></AutoSuggest>
+                <input ref={inputRef} className="form-control" value={listNameValue} onChange={handleChange("listName")} name="listName" aria-describedby="listNameHelp" />
+                <div id="listNameHelp" className="form-text">Your links are added to lists, please specify a list name</div>
+                {touched.listName && errors.listName ? <div className="text-danger"> {errors.listName} </div> : null}
+              </div>
+              <div className="mb-3">
+                <label for="listDescription" className="form-label">List Description</label>
+                <Field className="form-control" name="listDescription" aria-describedby="listDescriptionHelp" />
+                <div id="listDescriptionHelp" className="form-text">What kind of content is present in this list?</div>
+                {touched.listDescription && errors.listDescription ? <div className="text-danger"> {errors.listDescription} </div> : null}
+              </div>
+              <div className="mb-3">
+                 <label for="tag" className="form-label">Tag</label>
+                {/*<Field className="form-control" name="tag" aria-describedby="tagHelp" />
+                {touched.tag && errors.tag ? <div className="text-danger"> {errors.tag} </div> : null} */}
+                <div className="mb-3">
+                  <AutoSuggest placeholder=""></AutoSuggest>
+                </div>
+                <div id="tagHelp" className="form-text">Select a tag name that best describes your list</div>
+              </div>
+              <div className="mb-3">
+                <label for="url" className="form-label">URL</label>
+                <Field className="form-control" name="url" aria-describedby="urlHelp" />
+                <div id="urlHelp" className="form-text">Provide the link you want to share</div>
+                {touched.url && errors.url ? <div className="text-danger">{errors.url} </div> : null}
+              </div>
+              <div className="mb-3">
+                <label for="urlDescription" className="form-label">URL Description</label>
+                <Field className="form-control" name="urlDescription" aria-describedby="urlDescriptionHelp" />
+                <div id="urlDescriptionHelp" className="form-text">What is the link about?</div>
+                {touched.urlDescription && errors.urlDescription ? <div className="text-danger"> {errors.urlDescription}</div> : null}
+              </div>
+              <Button type="submit">Save</Button>
+            </Form>
+          )}
+        </Formik>
       </ModalBody>
       <ModalFooter>
         <Button onClick={props.handleClose}>Cancel</Button>
-        <Button>Save</Button>
       </ModalFooter>
     </Modal>
 
